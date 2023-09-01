91096b493d357e10575f382ae850b24cd1c27097
pvd232 <50126818+pvd232@users.noreply.github.com>
2023-09-01 16:21:23 -0400

    MediaStream class implemented and functional

--- MediaStream.ts
+++ MediaStream.ts
@@ -1,61 +1,39 @@
 import { getTwilioReply } from './audio_codec/getTwilioReply.ts';
+import { WebSocket } from 'uWebSockets.js';
+import WebSocketClient from 'ws';
+import { XIWebSocketResponse } from './XIWebSocketResponse.ts';
+
 import { log } from './log';
-
-interface WebSocket {
-  on(event: string, callback: (data?: any) => void): void;
-  send(data: string): void;
-  connect(url: string): void;
-}
-
-interface XIWebSocketResponse {
-  audio?: string;
-  isFinal?: boolean;
-  normalizedAlignment?: any;
-}
-
+import { TwilioUserData } from './UserData.ts';
 export class MediaStream {
-  private twilioWSConnection: WebSocket;
-  private xiWSClient: WebSocket;
-  private xiWSConnection?: WebSocket;
+  private twilioWSConnection: WebSocket<TwilioUserData>;
+  private xiWSClient: WebSocketClient;
   private streamSid: string;
+  private gptReply: string;
 
-  constructor(twilioWSConnection: WebSocket, xiWSClient: WebSocket) {
+  constructor(
+    twilioWSConnection: WebSocket<TwilioUserData>,
+    xiWSClient: WebSocketClient,
+    gptReply: string
+  ) {
     this.twilioWSConnection = twilioWSConnection;
     this.xiWSClient = xiWSClient;
     this.streamSid = '-1';
-
+    this.gptReply = gptReply;
     this.xiWSClient.on('connectFailed', (error: any) =>
       console.log('Connect Error: ' + error.toString())
     );
     this.xiWSClient.on('open', this.prepareWebsockets.bind(this));
-    this.xiWSClient.connect('ws://localhost:8888/ws-streaming-twilio');
   }
 
-  private prepareWebsockets(xiWSConnection: WebSocket) {
+  private prepareWebsockets() {
     console.log('WebSocket connected to eleven labs');
-    this.xiWSConnection = xiWSConnection;
-    this.xiWSConnection.on('message', this.handleMessageFromXI.bind(this));
-    this.xiWSConnection.on('error', console.error);
-    this.xiWSConnection.on('close', function (data) {
-      const event = JSON.parse(data);
-      console.log('event', event);
-      if (event.wasClean) {
-        console.info(
-          `Connection closed cleanly, code=${event.code}, reason=${event.reason}`
-        );
-      } else {
-        console.warn('Connection died');
-      }
+    this.xiWSClient.on('message', this.handleMessageFromXI.bind(this));
+    this.xiWSClient.on('error', console.error);
+    this.xiWSClient.on('close', function (data) {
+      console.log('WebSocket closed');
     });
-
-    // this.twilioWSConnection.on(
-    //   'message',
-    //   this.handleMessageFromTwilio.bind(this)
-    // );
-    // this.twilioWSConnection.on('close', this.close.bind(this));
-
-    // this.mode = 'recording';
-    this.streamSid = '-1';
+    this.initiateXICommunication(this.gptReply);
   }
 
   public initiateXICommunication(gptReply: string) {
@@ -71,28 +49,29 @@ export class MediaStream {
       },
       xi_api_key: process.env.ELEVEN_LABS_API_KEY,
     };
-    this.xiWSConnection!.send(JSON.stringify(streamInit));
+    this.xiWSClient!.send(JSON.stringify(streamInit));
     const textMessage = {
       text: formattedGptReply,
       try_trigger_generation: true,
     };
 
-    this.xiWSConnection!.send(JSON.stringify(textMessage));
+    this.xiWSClient!.send(JSON.stringify(textMessage));
 
     // 4. Send the EOS message with an empty string
     const eosMessage = {
       text: '',
     };
 
-    this.xiWSConnection!.send(JSON.stringify(eosMessage));
+    this.xiWSClient!.send(JSON.stringify(eosMessage));
   }
 
   private handleMessageFromXI(data: string) {
     let response: XIWebSocketResponse = JSON.parse(data);
+    console.log('response', response);
 
     if (response.audio) {
       const twilioReply = JSON.stringify(
-        getTwilioReply(response.audio, '1234')
+        getTwilioReply(response.audio, this.streamSid)
       );
       this.twilioWSConnection.send(twilioReply);
     } else {
--- index.ts
+++ index.ts
@@ -15,93 +15,26 @@ import { getGptReply } from './getGptReply.ts';
 import { convertToWav } from './audio_codec/convertToWav.ts';
 import { removeWavHeader } from './audio_codec/removeWavHeader.ts';
 import { getTwilioReply } from './audio_codec/getTwilioReply.ts';
+import { MediaStream } from './MediaStream.ts';
+import { TwilioUserData } from './UserData.ts';
 dotenv.config({ path: findConfig('.env') ?? undefined });
 
 const app = uWS.App();
+
 app.ws('/*', {
-  // Client opens new connection
-  open: (_ws: WebSocket<unknown>) => {
-    let startTime: Date;
-    console.log('WebSocket connected');
-    const voiceId = '21m00Tcm4TlvDq8ikWAM';
-    const model = 'eleven_monolingual_v1';
-    const wsClient = new WebSocketClient(
-      `wss://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream-input?model_type=${model}`
-    )
-      .on('open', async function open() {
-        console.log('WebSocket connected to eleven labs');
-        startTime = new Date();
-
-        const gptReply = await getGptReply(
-          "no we don't have it. it's on back order?"
-        );
-        const gptReplyDuration = new Date().getTime() - startTime.getTime();
-        console.log('gptReplyDuration', gptReplyDuration);
-
-        const formattedGptReply = gptReply + ' ';
-        const streamInit = {
-          text: ' ',
-          voice_settings: {
-            stability: 0.5,
-            similarity_boost: 0.75,
-          },
-          generation_config: {
-            chunk_length_schedule: [120, 160, 250, 290],
-          },
-          xi_api_key: process.env.ELEVEN_LABS_API_KEY,
-        };
-        wsClient.send(JSON.stringify(streamInit));
-        const textMessage = {
-          text: formattedGptReply,
-          try_trigger_generation: true,
-        };
-
-        wsClient.send(JSON.stringify(textMessage));
-
-        // 4. Send the EOS message with an empty string
-        const eosMessage = {
-          text: '',
-        };
-
-        wsClient.send(JSON.stringify(eosMessage));
-      })
-      .on('message', async function message(data: string) {
-        let response: XIWebSocketResponse = JSON.parse(data);
-        if (response.audio) {
-          const twilioReply = getTwilioReply(response.audio, '1234');
-
-          // Send the audio to Twilio
-        } else {
-          console.log('No audio data in the response');
-          console.log('response', response);
-        }
-        if (response.isFinal) {
-          console.log('Generation is complete');
-          const endTime = new Date();
-          const duration = endTime.getTime() - startTime.getTime();
-          console.log('duration', duration);
-          // the generation is complete
-        }
-        if (response.normalizedAlignment) {
-          console.log('Alignment info is available');
-          // use the alignment info if needed
-        }
-      })
-      .on('error', console.error)
-      .on('close', function (data: string) {
-        const event = JSON.parse(data);
-        console.log('event', event);
-        if (event.wasClean) {
-          console.info(
-            `Connection closed cleanly, code=${event.code}, reason=${event.reason}`
-          );
-        } else {
-          console.warn('Connection died');
-        }
-      });
+  // Twilio client opens new connection
+  open: async (ws: WebSocket<TwilioUserData>) => {
+    const gptReply = (await getGptReply('Hello')) ?? 'Hello';
+    new MediaStream(
+      ws,
+      new WebSocketClient(
+        `wss://api.elevenlabs.io/v1/text-to-speech/${process.env.ELEVEN_LABS_VOICE_ID}/stream-input?model_type=${process.env.ELEVEN_LABS_MODEL_ID}`
+      ),
+      gptReply
+    );
   },
   message: (
-    ws: WebSocket<unknown>,
+    ws: WebSocket<TwilioUserData>,
     message: ArrayBuffer,
     isBinary: boolean
   ) => {
